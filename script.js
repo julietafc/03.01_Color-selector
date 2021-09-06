8b65300ca52b439e5e1dbca4f7f96bbc4eb1013f
julietafc <juli47g1@stud.kea.dk>
2021-09-06 23:02:49 +0200

    rgb function working

--- script.js
+++ script.js
@@ -15,12 +15,11 @@ function start() {
 }
 
 // function when the user selects a new color
-
 function newColorSelected() {
   let color = colorInput.value;
   hexValue = color;
   rgbValue = hexToRgb(hexValue);
-  hslValue = rgbToHsl(rgbValue);
+  //   hslValue = rgbToHsl(rgbValue);
 
   displayColor(hexValue);
   displayValues(hexValue, rgbValue, hslValue);
@@ -41,29 +40,30 @@ function hexToRgb(hex) {
     green: green,
     blue: blue,
   };
-
   return rgbValue;
 }
 
 // function RGB TO HEX
-// function rgbToHex(rgbValue) {
-//   let red = rgbValue.red;
-//   let green = rgbValue.green;
-//   let blue = rgbValue.blue;
+function rgbToHex(rgbValue) {
+  let red = rgbValue.red;
+  let green = rgbValue.green;
+  let blue = rgbValue.blue;
 
-//   red = red.toString(16);
-//   green = green.toString(16);
-//   blue = blue.toString(16);
+  red = red.toString(16);
+  green = green.toString(16);
+  blue = blue.toString(16);
 
-//   const hexValue = "#" + red + green + blue;
-//   return hexValue;
-// }
+  const hexValue = "#" + red + green + blue;
+  return hexValue;
+}
+
 // function display color
 function displayColor(color) {
   document.querySelector(".colorDisplay").style.backgroundColor = color;
 }
 
 // function display color values
-function displayValues(HEX) {
+function displayValues(HEX, RGB) {
   document.querySelector(".HEX").textContent = HEX;
+  document.querySelector(".RGB").textContent = `R:${rgbValue.red}, G:${rgbValue.green}, B:${rgbValue.blue}`;
 }
