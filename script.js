73b8705215fbd27dda6111ca4b2e24aaf334fc35
julietafc <juli47g1@stud.kea.dk>
2021-09-06 20:48:45 +0200

    function start

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ let colorInput = document.querySelector(".colorPicker");
 let hexValue = document.querySelector(".HEX");
 let rgbValue = document.querySelector(".RGB");
 let hslValue = document.querySelector(".HSL");
+
+function start() {
+  // add eventlistener to color picker
+  colorInput.addEventListener("input", newColorSelected);
+  colorInput.addEventListener("change", newColorSelected);
+}
