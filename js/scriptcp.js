8e2e337665a0ed4b283c5ccd7a981e7018a07a38
julietafc <juli47g1@stud.kea.dk>
2021-09-09 18:57:49 +0200

    getBaseColor function

--- js/scriptcp.js
+++ js/scriptcp.js
@@ -9,3 +9,15 @@ function start() {
   const colorSelected = document.querySelector(".colorPicker");
   colorSelected.addEventListener("input", getBaseColor());
 }
+
+// Showing a selected color (possibly a delegator for the following function calls)
+function getBaseColor() {
+  console.log("getBaseColor");
+  const color = this.value;
+  const hex = color;
+  const rgb = hexToRgb(hex);
+  const hsl = rgbToHsl(rgb);
+
+  displayBaseValue(hex, rgb, hsl);
+  getHarmony(hsl); //  this function is called here or in rgbtohsl convertion?
+}
