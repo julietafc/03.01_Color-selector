8d1727aff5838f2ca3c679b8d1a73761e9a9ba62
julietafc <juli47g1@stud.kea.dk>
2021-09-09 19:17:54 +0200

    hextorgb function

--- js/scriptcp.js
+++ js/scriptcp.js
@@ -5,16 +5,16 @@ let harmony = document.getElementById("select").value;
 
 // Getting a selected color from the user
 function start() {
-  console.log("start");
-  const colorSelected = document.querySelector(".colorPicker");
-  colorSelected.addEventListener("input", getBaseColor());
+  let colorSelected = document.querySelector(".colorPicker");
+  colorSelected.addEventListener("input", getBaseColor);
+  colorSelected.addEventListener("change", getBaseColor);
 }
 
 // Showing a selected color (possibly a delegator for the following function calls)
 function getBaseColor() {
-  console.log("getBaseColor");
   const color = this.value;
   const hex = color;
+  console.log(hex);
   const rgb = hexToRgb(hex);
   const hsl = rgbToHsl(rgb);
 
@@ -31,11 +31,6 @@ function displayBaseValue(hex, rgb, hsl) {
   displayBaseColor(hex);
 }
 
-function displayBaseColor(color) {
-  console.log("displayBaseColor");
-  document.querySelector(".colorBaseDisplay").style.backgroundColor = color;
-}
-
 function hexToRgb(hex) {
   console.log(hex);
   let red = parseInt(hex.substring(1, 3), 16);
@@ -49,18 +44,49 @@ function hexToRgb(hex) {
   };
 }
 
-function getHarmony(hsl) {
-  console.log("getHarmony");
-  let harmony = document.getElementById("select").value;
-  switch (harmony) {
-    case "Analogous":
-      getAnalogous(hsl);
-      break;
-    default:
-      `Invalid`;
-  }
-}
+// function rgbToHex(rgb) {
+//   let red = rgb.red;
+//   let green = rgb.green;
+//   let blue = rgb.blue;
 
-function getAnalogous() {
-  console.log("getAnalogous");
-}
+//   red = red.toString(16);
+//   green = green.toString(16);
+//   blue = blue.toString(16);
+
+//   return { red, green, blue };
+// }
+
+// function displayHex(HEX) {
+//   console.log("displayHex");
+//   document.querySelector(".HEX").textContent = `HEX: ${HEX}`;
+// }
+
+// function displayRgb(RGB) {
+//   console.log("displayRgb");
+//   document.querySelector(".RGB").textContent = `RGB: ${RGB.red}, ${RGB.green}, ${RGB.blue} `;
+// }
+
+// function displayHsl(HSL) {
+//   document.querySelector(".HSL").textContent = `HSL: ${HSL.h}, ${HSL.s}, ${HSL.l}`;
+// }
+
+// function displayBaseColor(color) {
+//   console.log("displayBaseColor");
+//   document.querySelector(".colorBaseDisplay").style.backgroundColor = color;
+// }
+
+// function getHarmony(hsl) {
+//   console.log("getHarmony");
+//   let harmony = document.getElementById("select").value;
+//   switch (harmony) {
+//     case "Analogous":
+//       getAnalogous(hsl);
+//       break;
+//     default:
+//       `Invalid`;
+//   }
+// }
+
+// function getAnalogous() {
+//   console.log("getAnalogous");
+// }
