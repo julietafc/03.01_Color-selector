9896c7a575d84a6002b538f1d9b4574aeceb34b0
julietafc <juli47g1@stud.kea.dk>
2021-09-09 18:57:29 +0200

    start function

--- js/scriptcp.js
+++ js/scriptcp.js
@@ -1,1 +1,11 @@
 "use strict";
+
+window.addEventListener("load", start);
+let harmony = document.getElementById("select").value;
+
+// Getting a selected color from the user
+function start() {
+  console.log("start");
+  const colorSelected = document.querySelector(".colorPicker");
+  colorSelected.addEventListener("input", getBaseColor());
+}
